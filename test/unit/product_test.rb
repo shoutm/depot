fd74e40ccc91d6641b7e8ef7ba105e90bbdcd41f
Shouta Morimoto <shouta.morimoto@gmail.com>
2012-01-16 05:06:28 +0900

    add validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,4 +4,35 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:  products(:ruby).title,
+                          description: 'yyy',
+                          price:  1,
+                          image_url:  "fred.gif")
+
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+  end
+
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:  products(:ruby).title,
+                          description: 'yyy',
+                          price:  1,
+                          image_url:  "fred.gif")
+
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), 
+                  product.errors[:title].join('; ')
+  end
 end
